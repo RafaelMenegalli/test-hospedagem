0dac318f249c0c9f1269b8eaa416c67595a60001
Rafael Menegalli <rafaelmenegalli@gmail.com>
2024-07-10 22:22:59 -0300

    teste

---
+++ prisma/migrations/20240710225853_criando_model_products/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - You are about to alter the column `name` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(155)`.
+  - You are about to alter the column `email` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "name" SET DATA TYPE VARCHAR(155),
+ALTER COLUMN "email" SET DATA TYPE VARCHAR(255);
+
+-- CreateTable
+CREATE TABLE "Products" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(155) NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "description" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Products_pkey" PRIMARY KEY ("id")
+);
--- src/services/user/CreateUserService.ts
+++ src/services/user/CreateUserService.ts
@@ -8,9 +8,15 @@ interface CreateUserProps {
 class CreateUserService {
     async execute({ name, email }: CreateUserProps){
         try {
+            const id  = Math.floor(Math.random() * 100) + 1
+            const createdAt = new Date();
+            const updatedAt = new Date();
+
             const user = await prisma.user.create({
                 data: {
-                    name, email
+                    id,
+                    name,
+                    email
                 }
             })
 
